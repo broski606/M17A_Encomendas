eac81efa7eeec425f33d2835c12e50b002118cea
broski606 <ociganolelo@gmail.com>
2026-01-12 13:26:01 +0000

    Adicionada função de apagar ao formulário dos artigos

--- form_artigos.py
+++ form_artigos.py
@@ -10,7 +10,8 @@ class formArtigos(QtWidgets.QMainWindow,Ui_MainWindow):
 
         self.form_principal = form_principal
         self.pushButton_voltar.clicked.connect(self.voltar)
-        self.pushButton_6.clicked.connect(self.eliminarArtigo)
+        self.pushButton_6.clicked.connect(self.EliminarArtigo)
+        self.pushButton_2.clicked.connect(self.listagemartigos)
 
 
     def listagemartigos(self):
@@ -36,38 +37,42 @@ class formArtigos(QtWidgets.QMainWindow,Ui_MainWindow):
                 self.tableView.setEditTriggers(QtWidgets.QTableView.NoEditTriggers)
         except Exception as e:
             QtWidgets.QMessageBox.critical(self,"Erro",f"Ocorreu um erro:{e}")
-        
-    def eliminarArtigo(self): # Função (mas podia ser um método) que permite eliminar o registo selecionado na QTableView
+    
+    def LimparFiltro(self):
+        self.lineEdit.setText("") 
+        #self.lineEdit.clear()
+        self.listagemartigos()
+    
+    def EliminarArtigo(self):
         selecionados = self.tableView.selectionModel().selectedRows()
         if selecionados:
             linha = selecionados[0].row() # primeira linha selecionada
             modelo = self.tableView.model()
             id_artigo = modelo.data(modelo.index(linha, 0)) # Primeiro item da linha (identificador)
-            nome_artigo = modelo.data(modelo.index(linha, 1)) # Segundo item da linha (designação)
-            
+            nome_artigo = modelo.data(modelo.index(linha,1))
+
             conn_BD = ligacao_BD()
             if conn_BD and conn_BD!=-1:
-                cmd_sql = "SELECT COUNT(*) FROM artigo WHERE id_categoria = %s;"
-                num_registos = consultaUmValor(conn_BD, cmd_sql, (id_artigo,))
+                cmd_sql = "SELECT COUNT(*) FROM detalheencomenda WHERE idArtigo = %s;"
+                num_registos = consultaUmValor(conn_BD,cmd_sql,(id_artigo,))
                 if num_registos == 0:
-                    resposta = QtWidgets.QMessageBox.question(self, "Questão", f"Tem certeza de que deseja excluir a categoria com identificador {id_artigo} e designação{nome_artigo}?")
+                    resposta = QtWidgets.QMessageBox.question(self, "Questão", f"Tem certeza de que deseja excluir o artigo com identificador {id_artigo} e designação {nome_artigo}?")
                     if resposta == QtWidgets.QMessageBox.Yes:
-                        # Eliminar o registo 
-                        cmd_sql = "DELETE FROM artigos WHERE id = %s;"
-                        num_registos = operacao_DML(conn_BD, cmd_sql, (id_artigo,))
-                        if num_registos > 0:
-                            QtWidgets.QMessageBox.information(self, "Informação", "A eliminação do registo foi bem sucedida!")
-                            self.listagemcategorias()
-                        else:
-                            QtWidgets.QMessageBox.information(self, "Aviso", "Falha: Nenhum ficheiro foi eliminado!")
+                            # Eliminar o registo da BD
+                            cmd_sql = "DELETE FROM artigo WHERE id = %s;"
+                            num_registos= operacao_DML(conn_BD,cmd_sql,(id_artigo,))
+                            if num_registos > 0: 
+                                QtWidgets.QMessageBox.information(self, "Sucesso", "A eliminação do registo foi bem sucedida!")
+                                self.listagemCategorias()
+                            else:
+                                QtWidgets.QMessageBox.warning(self, "Aviso", "Nenhum registo foi eliminado !")
                     else:
-                        QtWidgets.QMessageBox.warning(self, "Aviso", "A eliminação do registo foicancelada!")
-                    pass
+                            QtWidgets.QMessageBox.warning(self, "Aviso", "A eliminação do registo foi cancelada!")
                 else:
-                    QtWidgets.QMessageBox.critical(self, "Aviso", f"Não é possível eliminar o artigo {nome_artigo} porque existem {num_registos} encomendas associadas.")
+                    QtWidgets.QMessageBox.warning(self,"Aviso",f"Não é possível eliminar o artigo {nome_artigo} pois existem encomendas associados a esse artigo!")
         else:
-            QtWidgets.QMessageBox.critical(self, "Aviso", "É necessário selecionar a linha da tabela da categoria a eliminar.")
-
+            QtWidgets.QMessageBox.warning(self,"Aviso","É necessário selecionar a linha da tabela que contém o registo do artigo a eliminar!")
+    
     def voltar(self):
         self.close()
         self.form_principal.show()
