9b835000a8060c66e4cf6cbbc44246b406934ffc
broski606 <ociganolelo@gmail.com>
2026-01-13 18:38:12 +0000

    Pequena correção no ficheiro de criação da base de dados

--- bdEncomendas_AndreSoares.sql
+++ bdEncomendas_AndreSoares.sql
@@ -1,22 +1,20 @@
-CREATE DATABASE bdEncomendas_T1;
+CREATE DATABASE bdencomendas_T1;
 
-USE bdEncomendas_T1;
+USE bdencomendas_T1;
 
-CREATE TABLE Categoria(
+CREATE TABLE categoria(
 id INT PRIMARY KEY,
 designacao VARCHAR(50) NOT NULL
 )engine=innodb;
 
-CREATE TABLE Artigo(
-id INT PRIMARY KEY,
-idCategoria INT,
-designacao VARCHAR(50) NOT NULL,
-preco DECIMAL(10,2),
-stock INT,
-FOREIGN KEY (idCategoria) REFERENCES Categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
+CREATE TABLE artigo(id INT PRIMARY KEY, 
+idCategoria INT, 
+designacao VARCHAR(50) NOT NULL, 
+preco DECIMAL(10,2), stock INT, 
+FOREIGN KEY (idCategoria) REFERENCES categoria(id) ON UPDATE CASCADE ON DELETE CASCADE
 )engine=innodb;
 
-CREATE TABLE Cliente(
+CREATE TABLE cliente(
 id INT PRIMARY KEY,
 nome VARCHAR(50) NOT NULL,
 morada VARCHAR(100) NOT NULL,
@@ -25,21 +23,21 @@ email VARCHAR(50) NOT NULL,
 nif INT
 )engine=innodb;
 
-CREATE TABLE Encomenda(
+CREATE TABLE encomenda(
 nEncomenda INT PRIMARY KEY,
 idCliente INT,
 dataEncomenda DATE,
 dataEntrega DATE,
-FOREIGN KEY (idCliente) REFERENCES Cliente(id) ON UPDATE CASCADE ON DELETE CASCADE
+FOREIGN KEY (idCliente) REFERENCES cliente(id) ON UPDATE CASCADE ON DELETE CASCADE
 )engine=innodb;
 
-CREATE TABLE DetalheEncomenda(
+CREATE TABLE detalheencomenda(
 nEncomenda INT NOT NULL,
 idArtigo INT NOT NULL,
 quantidade INT,
 PRIMARY KEY (nEncomenda, idArtigo),
-FOREIGN KEY (nEncomenda) REFERENCES Encomenda(nEncomenda) ON UPDATE CASCADE ON DELETE CASCADE,
-FOREIGN KEY (idArtigo) REFERENCES Artigo(id) ON UPDATE CASCADE ON DELETE CASCADE
+FOREIGN KEY (nEncomenda) REFERENCES encomenda(nEncomenda) ON UPDATE CASCADE ON DELETE CASCADE,
+FOREIGN KEY (idArtigo) REFERENCES artigo(id) ON UPDATE CASCADE ON DELETE CASCADE
 )engine=innodb;
 
 INSERT INTO `cliente`(`id`,`nome`,`email`,`telefone`,`nif`,`morada`) VALUES(1,'Lourenço Carneiro','lourencohomosexual@gaymail.pt','+351 969696969',111111111,'Rotunda da Boavista');
@@ -52,7 +50,7 @@ INSERT INTO `categoria`(`id`,`designacao`) VALUES(3,'Pesadas');
 
 INSERT INTO `artigo`(`id`,`idCategoria`,`designacao`,`stock`,`preco`) VALUES(1,2,'Cavalo do LeoFox',1,0);
 INSERT INTO `artigo`(`id`,`idCategoria`,`designacao`,`preco`,`stock`) VALUES(2,1,'Lourenço',100000,1);
-INSERT INTO `artigo`(`designacao`,`id`,`idCategoria`,`preco`,`stock`) VALUES('EGO DO LOURENCO CARNEIRO',3,3,9999999999,0);
+INSERT INTO `artigo`(`designacao`,`id`,`idCategoria`,`preco`,`stock`) VALUES('EGO DO LOURENCO CARNEIRO',3,3,99999999,0);
 
 INSERT INTO `encomenda`(`nEncomenda`,`idCliente`,`dataEncomenda`,`dataEntrega`) VALUES(1,2,'2023-09-11','2025-11-24');
 INSERT INTO `encomenda`(`nEncomenda`,`idCliente`,`dataEncomenda`,`dataEntrega`) VALUES(2,3,'2025-05-22','2025-11-24');
