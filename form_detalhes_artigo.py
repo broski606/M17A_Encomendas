551169b0dd393a1e42cd0cc663e58d8658ad41eb
broski606 <ociganolelo@gmail.com>
2026-01-15 20:15:48 +0000

    Afinal o form_detalhes_artigo.py não funcionava, mas agora já funciona na perfeição. Agora tenho a certeza

--- form_detalhes_artigo.py
+++ form_detalhes_artigo.py
@@ -20,58 +20,111 @@ class FormDetalhesArtigo(QtWidgets.QMainWindow,Ui_Form):
         self.pushButton_gravar.clicked.connect(self.gravar)
 
     def gravar(self):
-        try:
-            id = self.lineEdit_id.text()
-            designacao = self.lineEdit_designacao.text()
-            preco = self.lineEdit_preco.text()
-            stock = self.lineEdit_stock.text()
-            categoria = self.comboBox_categoria.currentText()
+        #alterado a partir do form_altear_categoria.py e do form_nova_categoria.py
+        if self.modo_funcionamento == "novo":
+            try:
+                id = self.lineEdit_id.text()
+                designacao = self.lineEdit_designacao.text()
+                preco = self.lineEdit_preco.text()
+                stock = self.lineEdit_stock.text()
+                categoria = self.comboBox_categoria.currentText()
 
-            if len(id)==0 or len(designacao)==0 or len(preco)==0 or len(stock)==0 or len(categoria)==0:
-                QtWidgets.QMessageBox.critical(self,"Aviso","Campos por preencher")
+                if len(id)==0 or len(designacao)==0 or len(preco)==0 or len(stock)==0 or len(categoria)==0:
+                    QtWidgets.QMessageBox.critical(self,"Aviso","Campos por preencher")
+                    return
+                tipoDados_id = verificar_tipo_dados(id)
+                if tipoDados_id != "inteiro":
+                    QtWidgets.QMessageBox.critical(self,"Aviso","Inserir valor inteiro no campo id")
+                    return
+                
+                conn_BD = ligacao_BD()
+                if not conn_BD:
+                    QtWidgets.QMessageBox.critical(self,"Erro","A ligação à BD não está estabelecida")
+                    return
+                cmd_sql = f"SELECT COUNT(*) FROM artigo WHERE id = %s OR designacao = %s;"
+                numRegistos = consultaUmValor(conn_BD, cmd_sql, (id, designacao,))
+                if numRegistos == -1:
+                    QtWidgets.QMessageBox.critical(self,"Erro","Ocorreu um erro ao verificar a existência da categoria")
+                    return
+                elif numRegistos > 0:
+                    QtWidgets.QMessageBox.critical(self,"Aviso", f"Já existe uma categoria com designação {designacao} ou com id {id} introduzidos")
+                    return
+                
+                cmd_sql = "SELECT id FROM categoria WHERE designacao = %s;"
+                idCategoria = consultaUmValor(conn_BD, cmd_sql, (categoria,))
+                if idCategoria == -1:
+                    QtWidgets.QMessageBox.critical(self,"Erro","Ocorreu um erro ao obter o ID da categoria")
+                    return
+                
+                cmd_sql = "INSERT INTO artigo (id, idCategoria, designacao, preco, stock) VALUES (%s, %s, %s, %s, %s);"
+                numRegistos = operacao_DML(conn_BD, cmd_sql, (id, idCategoria, designacao, preco, stock))
+                if numRegistos == -1:
+                    QtWidgets.QMessageBox.critical(self,"Erro","Ocorreu um erro ao inserir o registo")
+                    return
+                resposta = QtWidgets.QMessageBox.question(self, "Confirmação", "Categoria inserida com sucesso!\n Pretende inserir dados de uma nova categoria?", QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
+                if resposta == QtWidgets.QMessageBox.Yes:
+                    self.inicializar(selecao=None, modo_funcionamento="novo")
+                    return
+                else:
+                    self.voltar()
+                conn_BD.close()
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(self,"Erro",f"Erro: {e}")
                 return
-            tipoDados_id = verificar_tipo_dados(id)
-            if tipoDados_id != "inteiro":
-                QtWidgets.QMessageBox.critical(self,"Aviso","Inserir valor inteiro no campo id")
-                return
-            
-            conn_BD = ligacao_BD()
-            if not conn_BD:
-                QtWidgets.QMessageBox.critical(self,"Erro","A ligação à BD não está estabelecida")
-                return
-            cmd_sql = f"SELECT COUNT(*) FROM artigo WHERE id = %s OR designacao = %s;"
-            numRegistos = consultaUmValor(conn_BD, cmd_sql, (id, designacao,))
-            if numRegistos == -1:
-                QtWidgets.QMessageBox.critical(self,"Erro","Ocorreu um erro ao verificar a existência da categoria")
-                return
-            elif numRegistos > 0:
-                QtWidgets.QMessageBox.critical(self,"Aviso", f"Já existe uma categoria com designação {designacao} ou com id {id} introduzidos")
-                return
-            
-            cmd_sql = "SELECT id FROM categoria WHERE designacao = %s;"
-            idCategoria = consultaUmValor(conn_BD, cmd_sql, (categoria,))
-            if idCategoria == -1:
-                QtWidgets.QMessageBox.critical(self,"Erro","Ocorreu um erro ao obter o ID da categoria")
-                return
-            
-            cmd_sql = "INSERT INTO artigo (id, idCategoria, designacao, preco, stock) VALUES (%s, %s, %s, %s, %s);"
-            numRegistos = operacao_DML(conn_BD, cmd_sql, (id, idCategoria, designacao, preco, stock))
-            if numRegistos == -1:
-                QtWidgets.QMessageBox.critical(self,"Erro","Ocorreu um erro ao inserir o registo")
-                return
-            resposta = QtWidgets.QMessageBox.question(self, "Confirmação", "Categoria inserida com sucesso!\n Pretende inserir dados de uma nova categoria?", QtWidgets.QMessageBox.Yes | QtWidgets.QMessageBox.No)
-            if resposta == QtWidgets.QMessageBox.Yes:
-                self.inicializar()
-                return
-            else:
-                self.voltar()
-            conn_BD.close()
-        except Exception as e:
-            QtWidgets.QMessageBox.critical(self,"Erro",f"Erro: {e}")
-            return
         
+        elif self.modo_funcionamento == "alterar":
+            try:
+                id = self.lineEdit_id.text()
+                designacao = self.lineEdit_designacao.text()
+                preco = self.lineEdit_preco.text()
+                stock = self.lineEdit_stock.text()
+                categoria = self.comboBox_categoria.currentText()
+
+                if len(designacao)==0:
+                    QtWidgets.QMessageBox.critical(self,"Aviso","Designacao do artigo por preencher")
+                    return
+
+                if self.designacao == designacao:
+                    QtWidgets.QMessageBox.critical(self,"Aviso","A designação do artigo não foi alterada")
+                    return
+                
+                conn_BD = ligacao_BD()
+                if not conn_BD:
+                    QtWidgets.QMessageBox.critical(self,"Erro","A ligação à BD não está estabelecida")
+                    return
+                cmd_sql = f"SELECT COUNT(*) FROM artigo WHERE id = %s;"
+                numRegistosId = consultaUmValor(conn_BD, cmd_sql, (id,))
+                cmd_sql = f"SELECT COUNT(*) FROM artigo WHERE designacao = %s;"
+                numRegistosD = consultaUmValor(conn_BD, cmd_sql, (designacao,))
+                if numRegistosId == -1 or numRegistosD == -1:
+                    QtWidgets.QMessageBox.critical(self,"Erro","Ocorreu um erro ao verificar a existência do artigo")
+                    return
+                elif numRegistosD > 0:
+                    QtWidgets.QMessageBox.critical(self,"Aviso", f"Já existe um artigo com designação {designacao} ou com id {id} introduzidos")
+                    return
+                elif numRegistosId==0:
+                    QtWidgets.QMessageBox.critical(self,"Aviso", f"Não foi possível encontrar o artigo com identificador {id}!")
+                    return
+                idCategoria = consultaUmValor(conn_BD, "SELECT id FROM categoria WHERE designacao = %s;", (categoria,))
+                cmd_sql = "UPDATE artigo SET designacao = %s, preco = %s, stock = %s, idCategoria = %s WHERE id = %s;"
+                numRegistos = operacao_DML(conn_BD, cmd_sql, (designacao, preco, stock, idCategoria, id))
+                if numRegistos == -1:
+                    QtWidgets.QMessageBox.critical(self,"Erro","Ocorreu um erro ao alterar o registo")
+                    return
+                QtWidgets.QMessageBox.information(self, "Confirmação", "Artigo alterado com sucesso!")
+                
+                self.close()
+                self.form_artigos.show()
+                self.form_artigos.listagemartigos()
+                conn_BD.close()
+
+            except Exception as e:
+                QtWidgets.QMessageBox.critical(self,"Erro",f"Erro: {e}")
+                return
+            
 
     def inicializar(self, selecao, modo_funcionamento):
+        self.modo_funcionamento = modo_funcionamento
         if modo_funcionamento == "novo":
             self.lineEdit_designacao.setText("")
             self.lineEdit_preco.setText("")
