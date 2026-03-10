77ce02bc9fcdf9db4e1a8736670913accaf641f1
broski606 <ociganolelo@gmail.com>
2026-03-10 15:45:36 +0000

    Update na template da base de dados

--- bdEncomendas_AndreSoares.sql
+++ bdEncomendas_AndreSoares.sql
@@ -39,23 +39,3 @@ PRIMARY KEY (nEncomenda, idArtigo),
 FOREIGN KEY (nEncomenda) REFERENCES encomenda(nEncomenda) ON UPDATE CASCADE ON DELETE CASCADE,
 FOREIGN KEY (idArtigo) REFERENCES artigo(id) ON UPDATE CASCADE ON DELETE CASCADE
 )engine=innodb;
-
-INSERT INTO `cliente`(`id`,`nome`,`email`,`telefone`,`nif`,`morada`) VALUES(1,'Lourenço Carneiro','lourencohomosexual@gaymail.pt','+351 969696969',111111111,'Rotunda da Boavista');
-INSERT INTO `cliente`(`id`,`nome`,`email`,`telefone`,`nif`,`morada`) VALUES(2,'Gonçalo Moreira','ilovegothmommies@yahoo.pt','+256 912648955',998800712,'Cabanas de Goblins');
-INSERT INTO `cliente`(`nome`,`telefone`,`email`,`morada`,`id`,`nif`) VALUES('Leonardo Raposo','+351 919826583','ilovemyhorsedaddy@yahoo.com','Cemitério de Águas Santas',3,222222222);
-
-INSERT INTO `categoria`(`id`,`designacao`) VALUES(1,'Frágil');
-INSERT INTO `categoria`(`id`,`designacao`) VALUES(2,'Cavalo');
-INSERT INTO `categoria`(`id`,`designacao`) VALUES(3,'Pesadas');
-
-INSERT INTO `artigo`(`id`,`idCategoria`,`designacao`,`stock`,`preco`) VALUES(1,2,'Cavalo do LeoFox',1,0);
-INSERT INTO `artigo`(`id`,`idCategoria`,`designacao`,`preco`,`stock`) VALUES(2,1,'Lourenço',100000,1);
-INSERT INTO `artigo`(`designacao`,`id`,`idCategoria`,`preco`,`stock`) VALUES('EGO DO LOURENCO CARNEIRO',3,3,99999999,0);
-
-INSERT INTO `encomenda`(`nEncomenda`,`idCliente`,`dataEncomenda`,`dataEntrega`) VALUES(1,2,'2023-09-11','2025-11-24');
-INSERT INTO `encomenda`(`nEncomenda`,`idCliente`,`dataEncomenda`,`dataEntrega`) VALUES(2,3,'2025-05-22','2025-11-24');
-INSERT INTO `encomenda`(`nEncomenda`,`idCliente`,`dataEncomenda`,`dataEntrega`) VALUES(3,1,'2025-11-23','2025-11-24');
-
-INSERT INTO `detalheencomenda`(`nEncomenda`,`idArtigo`,`quantidade`) VALUES(1,2,1);
-INSERT INTO `detalheencomenda`(`nEncomenda`,`idArtigo`,`quantidade`) VALUES(2,1,1);
-INSERT INTO `detalheencomenda`(`nEncomenda`,`idArtigo`,`quantidade`) VALUES(3,3,1);
